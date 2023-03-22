abc30ea36654dcce24ea8166875613b6f8b59074
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-03-22 11:52:00 +0700

    feat: Modals context

---
+++ src/Contexts/Modals.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable security/detect-object-injection */
+import { FC, createContext, ReactNode, useCallback, useState } from 'react'
+
+type IMoveModal = (options: { id: string; x: number; y: number }) => void
+
+interface IModalsContext {
+	MoveModal: IMoveModal
+	Modals: Record<string, IModal>
+}
+
+export enum IStatus {
+	Open,
+	Closed,
+}
+
+export interface IModal {
+	x: number
+	y: number
+	status: IStatus
+}
+
+const ModalsContext = createContext<IModalsContext>({
+	MoveModal: () => undefined,
+	Modals: {},
+})
+
+export const ModalsProvider: FC<{ children: ReactNode }> = ({ children }) => {
+	const [Modals, SetModals] = useState<Record<string, IModal>>({})
+
+	const MoveModal: IMoveModal = useCallback(({ id, x, y }) => {
+		SetModals(prev => ({
+			...prev,
+			[id]: {
+				...prev[id],
+				x,
+				y,
+			},
+		}))
+	}, [])
+
+	return (
+		<ModalsContext.Provider value={{ Modals, MoveModal }}>
+			{children}
+		</ModalsContext.Provider>
+	)
+}
+
+export default ModalsContext
