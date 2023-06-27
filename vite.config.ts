4c9f7c43146870bd98ed41ddf2ea32382fb8f2a6
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-06-27 19:01:38 +0700

    chore: Linaria vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
+import linaria from '@linaria/vite'
 
 import { resolve } from 'path'
 
@@ -37,7 +38,7 @@ export default defineConfig(({ mode }) => {
 	const appEnv = loadEnv(mode, process.cwd(), envPrefix)
 
 	return {
-		plugins: [react(), eslintPlugin(), svgr()],
+		plugins: [react(), eslintPlugin(), svgr(), linaria()],
 		resolve: {
 			alias: resolveAlias,
 		},
